f2cda7c37ef496e65e996f0004c1d4b6faa7ff50
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-09 16:20:23 +0100

    creazione progetto con classi e application properties

---
+++ src/main/java/cristinapalmisani/U2W1L1/entities/Order.java
@@ -0,0 +1,54 @@
+package cristinapalmisani.U2W1L1.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.Random;
+
+@Getter
+@Component
+@Data
+@PropertySource("application.properties")
+public class Order {
+
+    private int orderNumber;
+    private Menu menu;
+    private Table table;
+    private OrderState orderState;
+    private int seats;
+    private LocalTime orderTime = LocalTime.now();
+    @Value("${order.seat}")
+    private double priceSeat;
+
+    public Order() {
+        orderNumber++;
+    }
+
+    public void setTable(Table table) {
+        Random rnd = new Random();
+        table.setAvailable(false);
+        this.table = table;
+        seats = rnd.nextInt(table.getSeats()) + 1;
+    }
+
+    public double getPriceSeat() {
+        return menu.getPrice() + seats * priceSeat;
+    }
+
+    public void printOrder() {
+        System.out.println("Order number: " + orderNumber);
+        System.out.println("Table number: " + table.getNumberTable());
+        System.out.println("Seats: " + seats + " / " + table.getSeats());
+        System.out.println("Order time: " + orderTime);
+        System.out.println("Order state: " + orderState);
+        System.out.println();
+        menu.getPizzasList().forEach(System.out::println);
+        menu.getDrinksList().forEach(System.out::println);
+        System.out.println("Order price: " + getPriceSeat());
+        System.out.println();
+    }
+}
